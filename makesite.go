cd5ae15fc752809eb7c352a3d897a3cedb194093
Anthony <sean.a.protho1@gmail.com>
2021-06-21 19:56:35 -0700

    starting 1.1

--- makesite.go
+++ makesite.go
@@ -9,20 +9,26 @@ import (
 )
 
 func main() {
-	// flag
+	// used $Go build && ./makesite --file=latest-post.txt
 	fileName := flag.String("file", "", "the file name to read")
+	// used $Go build && ./makesite --dir="./testDir"
+	dirName := flag.String("dir", "", "file directory")
+	// parses all created flags
 	flag.Parse()
 
+	// reads the contents of our file
 	fileContents, err := ioutil.ReadFile(*fileName)
 	if err != nil {
 		panic(err)
 	}
 
+	// creates a new file with the name of fileName.html
 	f, err := os.Create(strings.SplitN(*fileName, ".", 2)[0] + ".html")
 	if err != nil {
 		panic(err)
 	}
 
+	// parses old template into a new template to pass values
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 	err = t.Execute(f, string(fileContents))
 	if err != nil {
